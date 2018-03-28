841ade9cae0c1cbb25e548dbc5c5c8937fd93368
rcm32000 <rcm32000@yahoo.com>
2018-03-28 09:12:08 -0600

    Iteration 0 complete

---
+++ lib/patron.rb
@@ -0,0 +1,11 @@
+class Patron
+  attr_reader :name, :interests
+  def initialize(name)
+    @name       = name
+    @interests  = []
+  end
+
+  def add_interest(interest)
+    @interests << interest
+  end
+end
---
+++ test/patron-test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/patron.rb'
+
+class TestPatron < Minitest::Test
+  def test_class_exists
+    patron = Patron.new('Bob')
+
+    assert_instance_of Patron, patron
+  end
+
+  def test_name_and_zero_interests_default
+    patron = Patron.new('Bobby')
+
+    assert_equal 'Bobby', patron.name
+    assert_equal [], patron.interests
+  end
+
+  def test_add_interests
+    patron = Patron.new('Bonnie')
+    patron.add_interest('Dead Sea Scrolls')
+    patron.add_interest('Gems and Minerals')
+
+    expected = ['Dead Sea Scrolls', 'Gems and Minerals']
+    assert_equal expected, patron.interests
+  end
+end

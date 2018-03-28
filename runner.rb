96ab802c01f5a3e8b3d3a50a9964cf0f24681295
rcm32000 <rcm32000@yahoo.com>
2018-03-28 10:59:08 -0600

    Runner incomplete

--- runner.rb
+++ runner.rb
@@ -8,6 +8,7 @@ class Runner
   def start
     puts "What is the name of your museum?"
     input = gets.chomp
+    Museum.new(input)
     puts "Welcome to the Grand Opening of the #{input}!!!\n"\
          "What would you like to do?\n"\
          "(E) to add an exhibit\n"\
@@ -31,15 +32,49 @@ class Runner
 
   def input_valid?
     'e' || 'E' || 'a' || 'A' || 'r' || 'R' || 't' || 'T' || 'u' || 'U' ||
-    'q' || 'Q'
+      'q' || 'Q'
   end
 
   def evaluate_input(input)
     case input
-    when 'p', 'play' then choose_level
-    when 'q', 'quit' then quit_game.abort
-    when 'i', 'instructions' then start_info
+    when 'e', 'E' then append_exhibit
+    when 'a', 'A' then add_a_patron
+    when 'r', 'R' then check_revenue
+    when 't', 'T' then check_popularity
+    when 'u', 'U' then remove_unpopular
+    when 'q', 'Q' then quit.abort
     end
     input
+  end
+
+  def append_exhibit
+    puts 'What is the name of your exhibit and cost to vist it (ex: '\
+    'Dinosaurs, 10)?'
+    input = gets.chomp
+    until exhibit_valid? == true
+      add_exhibit(input)
+      puts 'Please include name and cost (ex: Dinosaurs, 10)'
+    end
+  end
+
+  def exhibit_valid?
+    input.include?(Integer)
+  end
+
+  def add_a_patron
+    puts 'What is the name of your patron?'
+    input = gets.chomp
+    Patron.new(input)
+    puts "What exhibit(s) does your patron enjoy?\n"\
+    'Please type (D)one when you are finished.'
+    until patron_valid? == true
+      patron.add_interest(input)
+      "What else are they interested in?\n"\
+      'Please type (D)one when you are finished.'
+    end
+  end
+
+  def patron_valid?
+    'd' || 'done'
   end
 end
